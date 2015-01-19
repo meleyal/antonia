431ffb7bbf3451ff691d5050237435e615033b62
William Meleyal <w.meleyal@wollzelle.com>
2015-01-19 12:02:20 +0100

    initial commit

---
+++ lib/import.rb
@@ -0,0 +1,7 @@
+require "rubygems"
+require "sequel"
+require "jekyll-import"
+
+JekyllImport::Importers::WordpressDotCom.run({ "source" => "/Users/william/Downloads/antoniachain.wordpress.2015-01-15.xml" })
+
+
