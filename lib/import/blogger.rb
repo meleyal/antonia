55b485dec1726d78ca69d41a8350a25d10441e1f
William Meleyal <william.meleyal@gmail.com>
2016-04-03 22:33:06 +0100

    new design

--- lib/import/blogger.rb
+++
@@ -1,7 +0,0 @@
-require "jekyll-import"
-
-JekyllImport::Importers::Blogger.run({
-  "source"                => "/path/to/blog-03-31-2016.xml",
-  "no-blogger-info"       => true,
-  "replace-internal-link" => true,
-})
